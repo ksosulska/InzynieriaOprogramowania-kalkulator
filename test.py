81c7a9cdea591c205522780fa9cbda8e2eb49494
PiotrPisz <101944150+PiotrPisz@users.noreply.github.com>
2022-03-29 18:40:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,12 @@
 def hello(name):
 	return "Hello +str(name)
 
+#Panie Kasiu to jest dzielenie
 def pomnoz(a,b)
 	resturn a/b
+	
+def mnozenie(a,b)
+	resturn a*b	
 
 def odejmij(a,b):
 	wynik = float(a) - float(b)
